775f8a1b3c641233a7010ed3ed81ce2a54aa6dcb
nileeka97 <nileekaabeynayake@gmail.com>
2023-11-08 12:54:34 +0530

    add outputs.tf

--- resources.tf
+++ resources.tf
@@ -137,8 +137,4 @@ resource "aws_instance" "myapp-ec2" {
 
 
 
-# Outputs
-# Define output variables to expose specific information about the created resources
-output "ec2_public_ip" {
-  value = aws_instance.myapp-ec2.public_ip # Output the public IP address of the EC2 instance
-}
+
