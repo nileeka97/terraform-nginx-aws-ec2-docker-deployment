9bc218e54ed7d0cfa7491289c6ef58e21da600ae
nileeka97 <nileekaabeynayake@gmail.com>
2023-12-20 14:26:18 +0530

    Prepared to TF cloud

--- providers.tf
+++ providers.tf
@@ -1,16 +1,12 @@
-
-
 terraform {
-  backend "s3" {
-    bucket     = "ixd-terraform-tfstate-bucket"
-    key        = "terraform-nginx-aws-ec2-docker-deployment/terraform.tfstate"
-    region     = "us-east-1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
   }
 }
 
-
 provider "aws" {
-  #region     = var.aws_region
-  #access_key = var.aws_access_key
- # secret_key = var.aws_secret_key
+  # Configuration options
 }
